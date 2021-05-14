bc568e65b2001b663837f4bdad90a75ca01f6b6b
Aboubacar OUATTARA <abouba181@gmail.com>
2021-05-14 03:25:25 +0000

    wip

--- resources/views/front/portfolio/partials/price.blade.php
+++ resources/views/front/portfolio/partials/price.blade.php
@@ -1,4 +1,9 @@
-<?php $position = \Stevebauman\Location\Facades\Location::get() ?>
+<?php try {
+    $position = \Stevebauman\Location\Facades\Location::get();
+} catch (\Exception $exception) {
+    report($exception);
+    $position = false;
+}?>
 @if($position)
     @if(\Illuminate\Support\Str::upper($position->countryCode) === 'MA') <strong>1600dhs/jour</strong> @else <strong>250€/jour</strong>@endif
 @else<strong>1600dhs/jour</strong>@endif
